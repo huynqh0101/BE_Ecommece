01285077fdfc20f7bcfa967b7c3192eb9aa26c7a
huynqh0101 <huynqh0101@gmail.com>
2025-03-30 11:51:40 +0700

    update user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get('info/:username')
+  async getUserInfo(@Param('username') username: string) {
+    return await this.userService.getUserInfo(username);
+  }
+
+  @Get('all')
+  async getAllUsers() {
+    return await this.userService.getAllUsers();
+  }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from './Schema/user.schema';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+
+  async getUserInfo(username: string) {
+    const user = await this.userModel.findOne({ username }).exec();
+    if (!user) {
+      return { msg: 'User not found', data: null };
+    }
+    return {
+      msg: 'Get info user successfully',
+      data: {
+        username: user.username,
+        id: user._id,
+        deletedAt: user.deletedAt,
+        createdAt: user.createdAt,
+        updatedAt: user.updatedAt,
+        amountCart: user.amountCart
+      }
+    };
+  }
+
+  async getAllUsers() {
+    const users = await this.userModel.find().exec();
+    return {
+      msg: 'Get all users successfully',
+      data: users
+    };
+  }
+}
